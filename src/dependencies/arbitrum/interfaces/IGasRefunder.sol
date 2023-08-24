f730d96dbe5a4833c5eec3f1ad27d74d16a6a1ce
Bartek Rutkowski <barrutko.eth@gmail.com>
2023-08-24 21:03:09 +0200

    feat: remove GasRefundEnabled

--- src/dependencies/arbitrum/interfaces/IGasRefunder.sol
+++ src/dependencies/arbitrum/interfaces/IGasRefunder.sol
@@ -12,25 +12,3 @@ interface IGasRefunder {
     uint256 calldataSize
   ) external returns (bool success);
 }
-
-abstract contract GasRefundEnabled {
-  /// @dev this refunds the sender for execution costs of the tx
-  /// calldata costs are only refunded if `msg.sender == tx.origin` to guarantee the value refunded relates to charging
-  /// for the `tx.input`. this avoids a possible attack where you generate large calldata from a contract and get over-refunded
-  modifier refundsGas(IGasRefunder gasRefunder) {
-    uint256 startGasLeft = gasleft();
-    _;
-    if (address(gasRefunder) != address(0)) {
-      uint256 calldataSize = 0;
-      // if triggered in a contract call, the spender may be overrefunded by appending dummy data to the call
-      // so we check if it is a top level call, which would mean the sender paid calldata as part of tx.input
-      // solhint-disable-next-line avoid-tx-origin
-      if (msg.sender == tx.origin) {
-        assembly {
-          calldataSize := calldatasize()
-        }
-      }
-      gasRefunder.onGasSpent(payable(msg.sender), startGasLeft - gasleft(), calldataSize);
-    }
-  }
-}
