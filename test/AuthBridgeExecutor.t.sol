67c0d77fad541dc432bd3931da91fb1f6a22edac
Sam MacPherson <xylom44@gmail.com>
2024-05-07 15:31:10 +0900

    fix license; add auth bridge unit test scaffold; add specific compiler settings in foundry.toml

---
+++ test/AuthBridgeExecutor.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+
+contract AuthBridgeExecutorTest is Test {
+
+    function setUp() public {
+    }
+
+}
